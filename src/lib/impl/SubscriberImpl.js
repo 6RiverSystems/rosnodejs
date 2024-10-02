641e42e32bd82268c1ddeb20d86a8aacc49e771a
Dimitri Nikitopoulos <dnikitopoulos@6river.com>
2024-10-02 14:44:05 -0400

    fix: revert for address

--- src/lib/impl/SubscriberImpl.js
+++ src/lib/impl/SubscriberImpl.js
@@ -320,7 +320,7 @@ class SubscriberImpl extends EventEmitter {
     });
 
     // open the socket at the provided address, port
-    client.connect({port: port, host: address, autoSelectFamily: false}, () => {
+    client.connect(port, address, () => {
       if (this.isShutdown()) {
         client.end();
         return;
