0f2f2dfa8ef87b3d9ff09d1be99e2ce313149ef9
Dimitri Nikitopoulos <dnikitopoulos@6river.com>
2025-01-06 12:23:14 -0500

    fix: typo

--- src/lib/RosNode.js
+++ src/lib/RosNode.js
@@ -115,7 +115,7 @@ class RosNode extends EventEmitter {
     if (!subImpl) {
       subImpl = new SubscriberImpl(options, this);
       this._subscribers[topic] = subImpl;
-      this.first = true;
+      this.firstSubscriber = true;
     }
 
     const sub = new Subscriber(subImpl);
